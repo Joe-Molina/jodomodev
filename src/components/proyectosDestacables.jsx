f8ec44ae9d2d07a0d36bddb675eddbe99b7fd468
Joe Molina <joedodaniljr123@gmail.com>
2024-01-04 13:50:24 -0400

    proyectos destacados faltan 2

--- src/components/proyectosDestacables.jsx
+++ src/components/proyectosDestacables.jsx
@@ -11,6 +11,16 @@ export const ProyectosDestacables = () => {
       ],
       cardTecnologias: ["JavaScript", "HTML", "CSS"],
     },
+    {
+      imgSrc: "projects/pharmaplant.png",
+      cardTitle: "Pharmaplant",
+      cardParrafos: [
+        "Sitio web diseñado para el laboratorio Phamplant, enfocado en dara conocer los productios y promociones de la empresa",
+        "el sitio web cumple la funcion de ser un catalogo online donde el usuario puede ver informacion importante sobre medicamentos y los componentes con los que estos se fabrican.",
+        "asi como un blog orientado a dar informacion sobre la salud.",
+      ],
+      cardTecnologias: ["JavaScript", "HTML", "CSS"],
+    },
   ];
 
   return (
