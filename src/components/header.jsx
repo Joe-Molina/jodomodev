cc0da82a3e3e257cd5c3497468cdb4594b3fcaca
Joe Molina <joedodaniljr123@gmail.com>
2024-03-13 12:01:47 -0400

    added proyectos page and aboutme page

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,18 +10,24 @@ export const Header = () => {
   const MenuOpen = getMenu ? (
     <article className="fixed w-screen h-full z-20 bg-black top-20 left-0 flex justify-center items-center text-white opacity-90">
       <nav className=" absolute flex flex-col justify-start items-center h-full   w-full text-center pt-16">
-        <a href="/MenuGalletas" className=" text-2xl py-10">
+        <a href="/" className=" text-2xl py-10 hover:text-indigo-500 transition">
           Inicio
         </a>
-        <a href="" className=" text-2xl py-10 ">
+        <a href="/aboutme" className=" text-2xl py-10 hover:text-indigo-500 transition">
           Sobre Mi
         </a>
-        <a href="" className=" text-2xl py-10  ">
+        <a href="/proyectos" className=" text-2xl py-10  hover:text-indigo-500 transition">
           Proyectos
         </a>
-        <a href="" className=" text-2xl py-10 ">
+        <a href="" className=" text-2xl py-10 hover:text-indigo-500 transition">
           Contacto
         </a>
+            <a
+              href="CVJOE.pdf"
+              className="text-indigo-500 py-3 px-16 border-2 flex border-indigo-500 rounded-lg hover:bg-indigo-500 hover:bg-opacity-20 transition hover:animate-pulse "
+            >
+              cv
+            </a>
       </nav>
     </article>
   ) : (
@@ -34,28 +40,28 @@ export const Header = () => {
       <nav className="w-full lg:justify-end lg:items-center lg:p-5 hidden lg:flex">
         <ul className="text-white flex justify-center items-center">
           <li>
-            <a href="" className="pr-6 hover:text-indigo-500 transition">
+            <a href="/" className="pr-6 hover:text-indigo-500 transition">
               Inicio
             </a>
           </li>
           <li>
-            <a href="" className="pr-6 hover:text-indigo-500 transition">
+            <a href="/aboutme" className="pr-6 hover:text-indigo-500 transition">
               Sobre mi
             </a>
           </li>
           <li>
-            <a href="" className="pr-6 hover:text-indigo-500 transition">
+            <a href="/proyectos" className="pr-6 hover:text-indigo-500 transition">
               Proyectos
             </a>
           </li>
           <li>
-            <a href="" className="pr-6 hover:text-indigo-500 transition ">
+            <a href="/contacto" className="pr-6 hover:text-indigo-500 transition ">
               Contacto
             </a>
           </li>
           <li className="">
             <a
-              href=""
+              href="CVJOE.pdf"
               className="text-indigo-500 py-3 px-8 border-2 flex border-indigo-500 rounded-lg hover:bg-indigo-500 hover:bg-opacity-20 transition hover:animate-pulse "
             >
               cv
